5fd79004ae2839486d42856b71a677541e024f6e
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2022-10-27 12:02:54 +0600

    2th ls

--- app/src/main/java/com/example/jk/MainActivity.kt
+++ app/src/main/java/com/example/jk/MainActivity.kt
@@ -1,14 +1,1 @@
 package com.example.jk
-
-fun main(){
-    val x1 = readLine()!!.toInt()
-    val x2 = readLine()!!.toInt()
-
-    try {
-        val res = x1 / x2
-    } catch (e: ArithmeticException){
-
-    }
-
-    println("result = $res")
-}
---
+++ app/src/main/java/com/example/jk/Switch.java
@@ -0,0 +1,21 @@
+package com.example.jk;
+
+public class Switch {
+
+    public static void main(String[] args) {
+        int x = 1;
+        switch (x) {
+            case 1:
+                System.out.println("first");
+                break;
+            case 2:
+                System.out.println("second");
+                break;
+            case 3:
+                System.out.println("thrid");
+            default:
+                System.out.println("another number");
+        }
+    }
+}
+
