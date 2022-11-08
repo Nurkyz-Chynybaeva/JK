fff3fff1839fb293d155fd032f0b11101139bcf8
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2022-11-08 13:57:02 +0600

    jk

--- app/src/main/java/com/example/jk/Switch.java
+++ app/src/main/java/com/example/jk/Switch.java
@@ -1,21 +1,21 @@
 package com.example.jk;
 
 public class Switch {
+    public static String letterCapitalize(String str){
+        StringBuilder result = new StringBuilder();
+        String[] toArray = str.split(" ");
 
-    public static void main(String[] args) {
-        int x = 1;
-        switch (x) {
-            case 1:
-                System.out.println("first");
-                break;
-            case 2:
-                System.out.println("second");
-                break;
-            case 3:
-                System.out.println("thrid");
-            default:
-                System.out.println("another number");
+        for(int i = 0; i < toArray.length; i++){
+            char[] wordString = toArray[i].toCharArray();
+            wordString[0] = Character.toUpperCase(wordString[0]);
+            result.append(new String(wordString)).append(" ");
         }
+        return result.toString().trim();
     }
+    public static void main(String[] args) {
+        System.out.println(letterCapitalize("i ran there"));
+    }
+
 }
+
 
