0a206bc73f5b9b3cdcb8eb9858fb3a4ff2e1f7ce
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2022-11-23 21:50:08 +0600

    jk

--- app/src/main/java/com/example/jk/MainActivity.kt
+++ app/src/main/java/com/example/jk/MainActivity.kt
@@ -1,1 +1,12 @@
 package com.example.jk
+fun main(){
+    val a1 = readLine()!!.toInt()
+    val a2 = readLine()!!.toInt()
+    try {
+        val res = a1 / a2
+    } catch (e: ArithmeticException){
+
+    }
+    println(res)
+}
+
--- app/src/main/java/com/example/jk/Switch.java
+++ app/src/main/java/com/example/jk/Switch.java
@@ -1,21 +1,21 @@
 package com.example.jk;
-
-public class Switch {
-    public static String letterCapitalize(String str){
-        StringBuilder result = new StringBuilder();
-        String[] toArray = str.split(" ");
-
-        for(int i = 0; i < toArray.length; i++){
-            char[] wordString = toArray[i].toCharArray();
-            wordString[0] = Character.toUpperCase(wordString[0]);
-            result.append(new String(wordString)).append(" ");
-        }
-        return result.toString().trim();
-    }
-    public static void main(String[] args) {
-        System.out.println(letterCapitalize("i ran there"));
-    }
-
-}
-
-
+//
+//public class Switch {
+//    public static String letterCapitalize(String str){
+//        StringBuilder result = new StringBuilder();
+//        String[] toArray = str.split(" ");
+//
+//        for(int i = 0; i < toArray.length; i++){
+//            char[] wordString = toArray[i].toCharArray();
+//            wordString[0] = Character.toUpperCase(wordString[0]);
+//            result.append(new String(wordString)).append(" ");
+//        }
+//        return result.toString().trim();
+//    }
+//    public static void main(String[] args) {
+//        System.out.println(letterCapitalize("i ran there"));
+//    }
+//
+//}
+//
+//
